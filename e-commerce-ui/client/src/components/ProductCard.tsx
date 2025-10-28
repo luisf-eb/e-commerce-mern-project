d65609c4776b0263faa5f226d8cc8d5d54b124d9
luisf-eb <lfeikb@gmail.com>
2025-10-28 19:00:11 -0300

    first commit

---
+++ e-commerce-ui/client/src/components/ProductCard.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { ProductType } from "@/types";
+
+const ProductCard = ({product}:{product:ProductType}) => {
+  return (
+    <div className=''>ProductCards</div>
+  )
+}
+
+export default ProductCard
