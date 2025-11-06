c816919153ecca0fdcd65d6204a3cc8b67cd112f
luisf-eb <lfeikb@gmail.com>
2025-11-06 16:24:11 -0300

    improve ProductCard and add ' View all products' link

--- e-commerce-ui/client/src/components/ProductList.tsx
+++ e-commerce-ui/client/src/components/ProductList.tsx
@@ -1,6 +1,7 @@
 import { ProductsType } from "@/types";
 import Categories from "./Categories";
 import ProductCard from "./ProductCard";
+import Link from "next/link";
 
 // TEMPORARY
 const products: ProductsType = [
@@ -114,17 +115,23 @@ const products: ProductsType = [
   },
 ];
 
-const ProductList = () => {
+const ProductList = ({ category }: { category: string }) => {
   return (
-    <div className='w-full'>
-        <Categories/>
-        <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-12">
-            {products.map(product => (
-                <ProductCard key={product.id} product={product}/>
-            ))}
-        </div>
+    <div className="w-full">
+      <Categories />
+      <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-12">
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+      <Link
+        href={category ? `/products/?category=${category}` : "/products"}
+        className="flex justify-end mt-4 underline text-sm text-gray-500"
+      >
+        View all products
+      </Link>
     </div>
-  )
-}
+  );
+};
 
 export default ProductList;
